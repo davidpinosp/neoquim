0bed6f1e696e15dd0f1d51dcd971232e43d3518f
davidpinosp <davidspinosp@gmail.com>
2023-08-25 17:55:51 -0400

    adding connection to contact us api andpopup to confirm

--- src/pages/Contactanos.js
+++ src/pages/Contactanos.js
@@ -1,7 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "../styles/Contactanos.css";
 
 const Contactanos = () => {
+  const popupRef = useRef(null);
+
+  // Event listener to close the popup when clicked outside
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (popupRef.current && !popupRef.current.contains(event.target)) {
+        setShowPopUp(false);
+        setbuttonActive(true);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -9,6 +26,8 @@ const Contactanos = () => {
     message: "",
   });
 
+  const [showPopUp, setShowPopUp] = useState(false);
+  const [buttonActive, setbuttonActive] = useState(true);
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -19,6 +38,40 @@ const Contactanos = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const text = {
+      subject: "Posible Prospecto de Negocio: " + formData.subject,
+
+      text:
+        "Nombre: " +
+        formData.name +
+        "\n\nEmail: " +
+        formData.email +
+        "\n\nMensaje : " +
+        formData.message,
+    };
+
+    fetch(
+      "https://clinquant-starship-81eb7c.netlify.app/.netlify/functions/app/send-email",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(text),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle API response
+        console.log(data);
+      })
+      .catch((error) => {
+        // Handle error
+        console.error(error);
+      });
+    setbuttonActive(false);
+    setShowPopUp(true);
+
     console.log(formData); // You can perform further actions, like sending the data to a server, here
   };
 
@@ -73,10 +126,31 @@ const Contactanos = () => {
               required
             />
           </div>
+
           <div className="form-button ">
-            <button className="hhtext-box-button " type="submit">
-              Enviar
-            </button>
+            {buttonActive && (
+              <button className="hhtext-box-button " type="submit">
+                Enviar
+              </button>
+            )}
+
+            {showPopUp && (
+              <div ref={popupRef} className="confirmation-popup">
+                <div className="popup-text">
+                  {
+                    "Su informacion ha sido enviada! \n\nUno de nuestros representantes estara en contacto pronto."
+                  }
+                </div>
+                <button
+                  className="hhtext-box-button"
+                  onClick={() => {
+                    setShowPopUp(false);
+                  }}
+                >
+                  OK
+                </button>
+              </div>
+            )}
           </div>
         </form>
       </div>
