c497bb33c5d94d19a43be43d197253f6d2ebb18d
davidpinosp <davidspinosp@gmail.com>
2023-09-07 15:49:04 -0400

    adding text alignment for mobile and removing auto zoom on input fields for safari.

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -54,57 +54,64 @@ function Landing() {
       </div>
 
       {/* product */}
-      <div className="top-space">
-        <div className="text-block ">
-          <img
-            src={products}
-            className="text-block-image"
-            alt="products picture"
-          />
+      <div className="small-screen-adjust">
+        <div className="top-space">
+          <div className="text-block ">
+            <img
+              src={products}
+              className="text-block-image"
+              alt="products picture"
+            />
 
-          <div className="text-block-content ">
-            <span className="text-block-content-title">
-              <h2>Soluciones Efectivas</h2>
-            </span>
-            <div>
-              Ofrecemos productos de calidad y efectividad que brindan las
-              mejores soluciones. Trabajamos en conjunto pensando en el
-              beneficio para nuestros clientes, distribuidores pequeños,
-              medianos y para las grandes empresas de los distintos sectores.
+            <div className="text-block-content ">
+              <span className="text-block-content-title">
+                <h2>Soluciones Efectivas</h2>
+              </span>
+              <div>
+                Ofrecemos productos de calidad y efectividad que brindan las
+                mejores soluciones. Trabajamos en conjunto pensando en el
+                beneficio para nuestros clientes, distribuidores pequeños,
+                medianos y para las grandes empresas de los distintos sectores.
+              </div>
             </div>
           </div>
-        </div>
-        {/* product */}
+          {/* product */}
 
-        <div className="text-block ">
-          <img
-            src={laboratorio}
-            className="text-block-image"
-            alt="lab picture"
-          />
-          <div className="text-block-content ">
-            <span className="text-block-content-title">
-              <h2>Innovacion Constante</h2>
-            </span>
-            Contamos con nuestro propio laboratorio interno lo cual nos da un
-            conocimiento de nuestros ingredientes y los verdaderos beneficios
-            que brindan nuestros productos. Continuamente evolucionamos con las
-            tendencias mundiales de la biotecnología y avances tecnológicos.
+          <div className="text-block ">
+            <img
+              src={laboratorio}
+              className="text-block-image"
+              alt="lab picture"
+            />
+            <div className="text-block-content ">
+              <span className="text-block-content-title">
+                <h2>Innovacion Constante</h2>
+              </span>
+              Contamos con nuestro propio laboratorio interno lo cual nos da un
+              conocimiento de nuestros ingredientes y los verdaderos beneficios
+              que brindan nuestros productos. Continuamente evolucionamos con
+              las tendencias mundiales de la biotecnología y avances
+              tecnológicos.
+            </div>
           </div>
-        </div>
 
-        <div className="text-block ">
-          <img src={coto} className="text-block-image" alt="products picture" />
+          <div className="text-block ">
+            <img
+              src={coto}
+              className="text-block-image"
+              alt="products picture"
+            />
 
-          <div className="text-block-content">
-            <span className="text-block-content-title">
-              <h2>Responsabilidad Social</h2>
-            </span>
-            Nuestros productos son elaborados pensando en el bienestar y futuro
-            de la comunidad y el medio ambiente. Solo ingredientes naturales,
-            sin conservantes sintéticos, sin agentes espumantes. Trabajamos con
-            las 5 R de Sustenatabilidad: reducir, reparar, recuperar, reutilizar
-            y reciclar. Libre de crueldad animal
+            <div className="text-block-content">
+              <span className="text-block-content-title">
+                <h2>Responsabilidad Social</h2>
+              </span>
+              Nuestros productos son elaborados pensando en el bienestar y
+              futuro de la comunidad y el medio ambiente. Solo ingredientes
+              naturales, sin conservantes sintéticos, sin agentes espumantes.
+              Trabajamos con las 5 R de Sustenatabilidad: reducir, reparar,
+              recuperar, reutilizar y reciclar. Libre de crueldad animal
+            </div>
           </div>
         </div>
       </div>
